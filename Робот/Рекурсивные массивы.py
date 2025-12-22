927b410a4fa637e4d102515da4042edf70f15096
jeka286 <navetop170@gmail.com>
2025-12-22 11:49:28 +0900

    Рекурсивные массивы

--- Робот/Рекурсивные массивы.py
+++ Робот/Рекурсивные массивы.py
@@ -54,57 +54,57 @@
 
 
 # Final boss
-def solve(maze, row=None, col=None, path=None):
-    if row is None:
-        # ищу стартовую позицию
-        for i in range(len(maze)):
-            for j in range(len(maze[0])):
-                if maze[i][j] == 's':
-                    # ищу все возможные пути, добавляя старт в начало
-                    result = solve(maze, i, j, [])
-                    # добавляю стартовую точку в каждый найденный путь
-                    for route in result:
-                        route.insert(0, f's({i},{j})')
-                    return result
-        return []
-
-    # если робот дошёл до цели (x) - добавляем этот путь в результат
-    if maze[row][col] == 'x':
-        return [path] if path else [[]]
-
-    maze[row] = maze[row][:col] + '#' + maze[row][col + 1:]  # помечаю клетку как посещённую
-
-    all_paths = []
-    directions = \
-        [(-1, 0, 'Up'),
-         (1, 0, 'Down'),
-         (0, -1, 'Left'),
-         (0, 1, 'Right')]
-
-    for x, y, direction in directions:
-        new_row, new_col = row + x, col + y
-
-        # проверяю, можно ли двигаться в эту клетку
-        if (0 <= new_row < len(maze) and 0 <= new_col < len(maze[0]) and
-                maze[new_row][new_col] != '#'):
-            # рекурсивно ищу пути из новой позиции
-            additional_paths = solve(maze, new_row, new_col, path + [direction])
-            all_paths.extend(additional_paths)  # добавляю найденные пути к общему списку
-
-    # восстановление оригинального значения клетки для других ветвей поиска
-    # maze[row] = maze[row][:col] + orig + maze[row][col + 1:]
-    return all_paths
-
-maze = [
-    's----',
-    '##---',
-    '---##',
-    '----x'
-]
-
-result = solve(maze)
-for i in result:
-    print(i)
+# def solve(maze, row=None, col=None, path=None):
+#     if row is None:
+#         # ищу стартовую позицию
+#         for i in range(len(maze)):
+#             for j in range(len(maze[0])):
+#                 if maze[i][j] == 's':
+#                     # ищу все возможные пути, добавляя старт в начало
+#                     result = solve(maze, i, j, [])
+#                     # добавляю стартовую точку в каждый найденный путь
+#                     for route in result:
+#                         route.insert(0, f's({i},{j})')
+#                     return result
+#         return []
+#
+#     # если робот дошёл до цели (x) - добавляем этот путь в результат
+#     if maze[row][col] == 'x':
+#         return [path] if path else [[]]
+#
+#     maze[row] = maze[row][:col] + '#' + maze[row][col + 1:]  # помечаю клетку как посещённую
+#
+#     all_paths = []
+#     directions = \
+#         [(-1, 0, 'Up'),
+#          (1, 0, 'Down'),
+#          (0, -1, 'Left'),
+#          (0, 1, 'Right')]
+#
+#     for x, y, direction in directions:
+#         new_row, new_col = row + x, col + y
+#
+#         # проверяю, можно ли двигаться в эту клетку
+#         if (0 <= new_row < len(maze) and 0 <= new_col < len(maze[0]) and
+#                 maze[new_row][new_col] != '#'):
+#             # рекурсивно ищу пути из новой позиции
+#             additional_paths = solve(maze, new_row, new_col, path + [direction])
+#             all_paths.extend(additional_paths)  # добавляю найденные пути к общему списку
+#
+#     # восстановление оригинального значения клетки для других ветвей поиска
+#     # maze[row] = maze[row][:col] + orig + maze[row][col + 1:]
+#     return all_paths
+#
+# maze = [
+#     's----',
+#     '##---',
+#     '---##',
+#     '----x'
+# ]
+#
+# result = solve(maze)
+# for i in result:
+#     print(i)
 
 
 
